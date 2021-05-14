629fbb2836def3dcbcb242830c444baebc3353b4
Ramiz Amad <ramiz.amad99@gmail.com>
2021-05-14 17:06:13 +0100

    Adding all basic endpoints for getting all prices

--- cryptocomparestream/src/app.controller.ts
+++ cryptocomparestream/src/app.controller.ts
@@ -6,7 +6,32 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getBTC() {
+    return this.appService.getBTC();
+  }
+
+  @Get()
+  getETH() {
+    return this.appService.getETC();
+  }
+
+  @Get()
+  getXRP() {
+    return this.appService.getXRP();
+  }
+
+  @Get()
+  getLTC() {
+    return this.appService.getLTC();
+  }
+
+  @Get()
+  getBCH() {
+    return this.appService.getBCH();
+  }
+  
+  @Get()
+  getETC() {
+    return this.appService.getETC();
   }
 }
--- cryptocomparestream/src/app.service.ts
+++ cryptocomparestream/src/app.service.ts
@@ -1,8 +1,50 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, HttpService } from '@nestjs/common';
+import { map } from 'rxjs/operators';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+
+  constructor(private http: HttpService){}
+
+  getBTC(){
+    return this.http.get('https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,GBP,EUR,JPY,ZAR')
+      .pipe(
+        map(response => response.data)
+      )
+  }
+
+  getETH(){
+    return this.http.get('https://min-api.cryptocompare.com/data/price?fsym=ETH&tsyms=USD,GBP,EUR,JPY,ZAR')
+      .pipe(
+        map(response => response.data)
+      )
+  }
+
+  getXRP(){
+    return this.http.get('https://min-api.cryptocompare.com/data/price?fsym=XRP&tsyms=USD,GBP,EUR,JPY,ZAR')
+      .pipe(
+        map(response => response.data)
+      )
+  }
+
+  getLTC(){
+    return this.http.get('https://min-api.cryptocompare.com/data/price?fsym=LTC&tsyms=USD,GBP,EUR,JPY,ZAR')
+      .pipe(
+        map(response => response.data)
+      )
+  }
+
+  getBCH(){
+    return this.http.get('https://min-api.cryptocompare.com/data/price?fsym=BCH&tsyms=USD,GBP,EUR,JPY,ZAR')
+      .pipe(
+        map(response => response.data)
+      )
+  }
+
+  getETC(){
+    return this.http.get('https://min-api.cryptocompare.com/data/price?fsym=ETC&tsyms=USD,GBP,EUR,JPY,ZAR')
+      .pipe(
+        map(response => response.data)
+      )
   }
 }
