edad724c4d8f2487501642c14a78e9cb4d0db082
Ramiz Amad <ramiz.amad99@gmail.com>
2021-05-15 20:02:58 +0100

    Updating unit tests, backend

--- cryptocomparestream/src/app.service.ts
+++ cryptocomparestream/src/app.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, HttpService } from '@nestjs/common';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 @Injectable()
@@ -6,7 +7,7 @@ export class AppService {
 
   constructor(private http: HttpService){}
 
-  getPrices(crypto: string){
+  getPrices(crypto: string): Observable<any>{
     return this.http.get(`https://min-api.cryptocompare.com/data/price?fsym=${crypto}&tsyms=USD,GBP,EUR,JPY,ZAR`)
       .pipe(
         map(response => response.data)
