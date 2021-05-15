261665b9fe046bace53744cd928315d50cdea793
Ramiz Amad <ramiz.amad99@gmail.com>
2021-05-15 21:02:12 +0100

    Updated endpoint to take symb from endpoint params

--- cryptocomparestream/src/app.controller.ts
+++ cryptocomparestream/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,38 +6,11 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   /**
-   * Prod changes: include ':crypto' in the endpoint URL params, which will mean only one endpoint replaces all the below
    * Prod addition: having a endpoint that will check health of services; to check if NestJS server is alive, if cryptocompare is working etc.
    */
 
-  
-  @Get('btc')
-  getBTC() {
-    return this.appService.getPrices('btc');
-  }
-
-  @Get('eth')
-  getETH() {
-    return this.appService.getPrices('eth');
-  }
-
-  @Get('xrp')
-  getXRP() {
-    return this.appService.getPrices('xrp');
-  }
-
-  @Get('ltc')
-  getLTC() {
-    return this.appService.getPrices('ltc');
-  }
-
-  @Get('bch')
-  getBCH() {
-    return this.appService.getPrices('bch');
-  }
-  
-  @Get('etc')
-  getETC() {
-    return this.appService.getPrices('etc');
-  }
+   @Get('crypto/:symb')
+   getCryptoPrice(@Param('symb') symb) {
+     return this.appService.getPrices(symb);
+   }
 }
